ae939c1db8bccd4e9888319a602909e0aa37c216
volant97 <volant97@naver.com>
2024-04-13 04:45:46 +0900

    [TEST] API

---
+++ src/pages/TestPage2.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+type productsType = {
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  id: number;
+  images: string[];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+};
+
+function TestPage2() {
+  // const [products, setProducts] = useState<productsType[]>([]);
+  const [products, setProducts] = useState<any>([]);
+  const searched = "9";
+
+  // skip +10으로 페이지네이션
+  const fetchData = () => {
+    // fetch(`https://dummyjson.com/products/search?q=${searched}`)
+    fetch(`https://dummyjson.com/products?limit=10&skip=0&select=title,price`)
+      .then((res) => res.json())
+      .then((obj) => setProducts(obj.products));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(products);
+
+  return (
+    <>
+      {products.map((item: any, index: number) => (
+        <div key={index}>
+          <p>{item.title}</p>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default TestPage2;
--- src/shared/Router.tsx
+++ src/shared/Router.tsx
@@ -1,6 +1,7 @@
 import Home from "../pages/Home";
 import TestPage from "../pages/TestPage";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import TestPage2 from "../pages/TestPage2";
 
 function Router() {
   return (
@@ -8,6 +9,7 @@ function Router() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/test" element={<TestPage />} />
+        <Route path="/test2" element={<TestPage2 />} />
         <Route path="*" element={<Navigate replace to={"/"} />} />
       </Routes>
     </BrowserRouter>
